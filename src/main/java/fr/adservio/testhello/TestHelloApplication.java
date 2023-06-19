6aa900e08968e4acd8eda6547f091ea592be5652
jean-claude.mbeng <jean-claude.mbeng@adservio.fr>
2023-06-19 17:32:00 +0200

    Mon premier commit

---
+++ src/main/java/fr/adservio/testhello/TestHelloApplication.java
@@ -0,0 +1,13 @@
+package fr.adservio.testhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHelloApplication.class, args);
+    }
+
+}
